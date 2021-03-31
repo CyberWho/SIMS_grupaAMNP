83f3d448175feeca18bb1a40a50f352c582463a9
aleksandramitro <64558124+aleksandramitro@users.noreply.github.com>
2021-03-31 19:08:21 +0200

    gotov read

--- Hospital/Hospital/xaml_windows/Patient/PatientAppointments.xaml.cs
+++ Hospital/Hospital/xaml_windows/Patient/PatientAppointments.xaml.cs
@@ -11,6 +11,10 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data;
+using Oracle.ManagedDataAccess.Client;
+using Oracle.ManagedDataAccess.Types;
+using System.Configuration;
 
 namespace Hospital.xaml_windows.Patient
 {
@@ -20,10 +24,65 @@ namespace Hospital.xaml_windows.Patient
     public partial class PatientAppointments : Window
     {
         int id;
+        OracleConnection con = null;
         public PatientAppointments(int id)
         {
+            this.setConnection();
             InitializeComponent();
-            this.id = id;
+            this.id = id; 
+        }
+
+        private void setConnection()
+        {
+            String conString = "User Id = ADMIN; password = Passzacloud1.; Data Source = dbtim1_high;";
+            con = new OracleConnection(conString);
+            try
+            {
+                con.Open();
+                MessageBox.Show("Connected");
+            }
+            catch (Exception exp)
+            {
+
+            }
+        }
+
+        private int getPatientId()
+        {
+            int patient_id = 0;
+            OracleCommand cmd = con.CreateCommand();
+            cmd.CommandText = "SELECT * FROM PATIENT";
+            cmd.Parameters.Add(new OracleParameter("id", id));
+            cmd.CommandType = CommandType.Text;
+            OracleDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                if (id == int.Parse(dr.GetString(4)))
+                {
+                    //MessageBox.Show("IMA BOGA");
+                    patient_id = int.Parse(dr.GetString(0));
+                    
+                }
+            }
+            
+            return patient_id;
+        }
+
+        private void updateDataGrid()
+        {
+            
+            int patient_id = getPatientId();
+            OracleCommand cmd = con.CreateCommand();
+            cmd.CommandText = "SELECT APPOINTMENT.DURATIONS_MINS,APPOINTMENT.DATE_TIME,APPOINTMENT.ROOM_ID,USERS.NAME,USERS.SURNAME FROM APPOINTMENT,DOCTOR,EMPLOYEES,USERS WHERE APPOINTMENT.PATIENT_ID = :patient_id AND APPOINTMENT.DOCTOR_ID = DOCTOR.ID AND DOCTOR.EMPLOYEE_ID = EMPLOYEES.ID AND EMPLOYEES.USER_ID = USERS.ID";
+            cmd.Parameters.Add(new OracleParameter("patient_id", patient_id));
+            cmd.CommandType = CommandType.Text;
+            OracleDataReader drugi = cmd.ExecuteReader();
+
+            DataTable dt = new DataTable();
+            dt.Load(drugi);
+            myDataGrid.ItemsSource = dt.DefaultView;
+            drugi.Close();
+
         }
 
         private void MojProfil_Click(object sender, RoutedEventArgs e)
@@ -45,6 +104,16 @@ namespace Hospital.xaml_windows.Patient
             var s = new PatientUI(id);
             s.Show();
             this.Close();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            con.Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.updateDataGrid();
         }
     }
 }
