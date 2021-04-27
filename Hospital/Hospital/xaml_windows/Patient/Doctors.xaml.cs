8ca5ecf39d29f65b5d16eb459135e9943b4d73c2
aleksandramitro <64558124+aleksandramitro@users.noreply.github.com>
2021-04-27 19:39:29 +0200

    pocela sam da radim pregled doktora za ocenjivanje

---
+++ Hospital/Hospital/xaml_windows/Patient/Doctors.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Hospital.Model;
+using Hospital.Controller;
+using System.Collections.ObjectModel;
+
+namespace Hospital.xaml_windows.Patient
+{
+    /// <summary>
+    /// Interaction logic for Doctors.xaml
+    /// </summary>
+    public partial class Doctors : Window
+    {
+        private int userId;
+        PatientController patientController = new PatientController();
+        System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+        ReminderController reminderController = new ReminderController();
+        public Doctors(int userId)
+        {
+            InitializeComponent();
+            this.userId = userId;
+        }
+        private void dispatherTimer_Tick(object sender, EventArgs e)
+        {
+            ObservableCollection<Reminder> reminders = new ObservableCollection<Reminder>();
+            Hospital.Model.Patient patient = new Model.Patient();
+            patient = patientController.GetPatientByUserId(userId);
+            reminders = reminderController.GetAllFutureRemindersByPatientId(patient.Id);
+            DateTime now = DateTime.Now;
+            now = now.AddMilliseconds(-now.Millisecond);
+            foreach (Reminder reminder in reminders)
+            {
+                if ((reminder.AlarmTime - now).Minutes == 0)
+                {
+                    MessageBox.Show(reminder.Description);
+                }
+            }
+        }
+        private void MojProfil_Click(object sender, RoutedEventArgs e)
+        {
+            var s = new PatientInfo(userId);
+            s.Show();
+            this.Close();
+        }
+
+        private void MojiPregledi_Click(object sender, RoutedEventArgs e)
+        {
+            var s = new PatientAppointments(userId);
+            s.Show();
+            this.Close();
+        }
+
+        private void PocetnaStranica_Click(object sender, RoutedEventArgs e)
+        {
+            var s = new PatientUI(userId);
+            s.Show();
+            this.Close();
+        }
+        private void Doktori_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void ZdravstveniKarton_Click(object sender, RoutedEventArgs e)
+        {
+            var s = new PatientHealthRecord(userId);
+            s.Show();
+            this.Close();
+        }
+        private void MojiPodsetnici_Click(object sender, RoutedEventArgs e)
+        {
+            var s = new PatientReminders(userId);
+            s.Show();
+            this.Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            dispatcherTimer.Tick += dispatherTimer_Tick;
+            dispatcherTimer.Interval = new TimeSpan(0, 1, 0);
+            dispatcherTimer.Start();
+        }
+    }
+}
