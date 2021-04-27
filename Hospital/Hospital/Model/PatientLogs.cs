427424d2e0e1ce1c7fca1d81494ad154fb0beaef
aleksandramitro <64558124+aleksandramitro@users.noreply.github.com>
2021-04-27 13:37:38 +0200

    dodato polje lastCounterReset u PatientLogs

--- Hospital/Hospital/Model/PatientLogs.cs
+++ Hospital/Hospital/Model/PatientLogs.cs
@@ -11,17 +11,19 @@ namespace Hospital.Model
         public int Id { get; set; }
         public int LogCounter { get; set; }
 
+        public DateTime LastCounterReset { get; set; }
         public Patient patient { get; set; }
 
         public PatientLogs()
         {
 
         }
-        public PatientLogs(int id,int logCounter,Patient patient)
+        public PatientLogs(int id,int logCounter,Patient patient,DateTime lastCounterReset)
         {
             this.Id = id;
             this.LogCounter = logCounter;
             this.patient = patient;
+            this.LastCounterReset = lastCounterReset;
         }
     }
 }
